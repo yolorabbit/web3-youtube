d679f540473ddc56caeb92dd4d1eade479f9eb1e
yologolf <surprise.eng000@gmail.com>
2022-11-13 19:38:59 -0300

    fix: fixes contract import

--- utils/contract.js
+++ utils/contract.js
@@ -1,4 +1,4 @@
-import ContractAbi from "../artifacts/contracts/YouTube.sol/YouTube.json";
+import ContractAbi from "../artifacts/contracts/Youtube.sol/YouTube.json";
 import { ethers } from "ethers";
 import { CONTRACT_ADDRESS } from "../constants";
 
